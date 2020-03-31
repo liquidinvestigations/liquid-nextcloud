02889884461bab33f66fe870421b74278bfa65ea
Morten Stehr <morten.stehr@spiegel.de>
2020-03-31 18:33:52 +0200

    changes pg

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ if [ "$INSTALLED" == "false" ]; then
     php /var/www/html/occ maintenance:install \
             --no-interaction \
             --verbose \
-            --database pgsql \
+            --database postgresql \
             --database-name $POSTGRES_DB \
             --database-host $POSTGRES_HOST \
             --database-user $POSTGRES_USER \
